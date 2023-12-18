058c636f9892a43402627ce8f6239c6d930cac13
Nikita Peretyagin <96194488+Peretyagin@users.noreply.github.com>
2023-12-18 14:09:04 +0300

    Add files via upload

---
+++ veb-resurss.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/run_process', methods=['POST'])
+def run_process():
+    process_name = request.form['notepad.exe']
+    try:
+        subprocess.Popen(process_name)
+        return 'Process {} started'.format(process_name)
+    except Exception as e:
+        return 'Error starting process {}: {}'.format(process_name, str(e))
+
+if __name__ == '__main__':
+    app.run()
+
+
+
